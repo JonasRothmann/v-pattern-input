0705fdee6cb133be8aad85fe44f92af537e3d6ee
JonasRothmann <jonasrothmannhjalager@gmail.com>
2020-01-21 15:14:17 +0100

    changed resolve url in module.js

--- nuxt/module.js
+++ nuxt/module.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 
 export default function(moduleOptions) {
   this.addPlugin({
-    src: resolve(__dirname, 'debug.js'),
+    src: resolve(__dirname, 'v-pattern-input.template.js'),
     fileName: 'v-pattern-input'
   })
 }
