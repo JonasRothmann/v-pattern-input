e041e134ee53ffaabb627a12d366e92a95c887c5
JonasRothmann <jonasrothmannhjalager@gmail.com>
2020-01-21 14:57:59 +0100

    Setup

---
+++ nuxt/module.js
@@ -0,0 +1,10 @@
+const { resolve } = require('path')
+
+export default function(moduleOptions) {
+  this.addPlugin({
+    src: resolve(__dirname, 'debug.js'),
+    fileName: 'v-pattern-input'
+  })
+}
+
+module.exports.meta = require('./package.json')
---
+++ nuxt/v-pattern-input.template.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import VPatternInput from '../src/components/v-pattern-input.vue'
+
+// loop through components and register them
+Vue.component('v-pattern-input', {
+  extends: VPatternInput
+})
